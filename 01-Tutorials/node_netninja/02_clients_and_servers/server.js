d35d60ee2191bfe3c0d09ecddaaa8ad0783b7672
Rhythm Mittal <rhythmmittal19@gmail.com>
2025-11-21 15:55:12 +0530

    progress in nodejs

--- 01-Tutorials/node_netninja/02_clients_and_servers/server.js
+++ 01-Tutorials/node_netninja/02_clients_and_servers/server.js
@@ -1,6 +1,15 @@
 const http = require("http"); // to handle http
 const fs = require("fs"); //file system to access html pages
 
+// STATUS CODE - describe the type of response sent to the browser
+/*
+1XX - Informational Resonses
+2XX - Success Codes
+3XX - Codes For Redirects
+4XX - Client Side Error ( NOT FOUND )
+5XX - Server Side Error
+*/
+
 // if a guest comes at the door the things the person has to do or provide
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
@@ -21,16 +30,24 @@ const server = http.createServer((req, res) => {
   switch (req.url) {
     case "/":
       path += "index.html";
+      res.statusCode = 200;
       break;
     case "/about":
       path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader('Location', '/about');
+      res.end();
       break;
     default:
       path += "404.html";
+      res.statusCode = 404;
       break;
   }
 
-  // send a html file
+  // send a html file and read them via file system
   fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
